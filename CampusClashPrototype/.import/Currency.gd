8303f98d697150faf0b7dd4c127d961e72526827
Benjamin Hines <benrh99@yahoo.com>
2021-10-01 15:46:22 -0500

    We got two timers baby!!!

--- CampusClashPrototype/.import/Currency.gd
+++ CampusClashPrototype/.import/Currency.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-var CurrencyDisplay = 0
+var HumanCurrencyDisplay = 0
 
-onready var timer = get_node("Timer")
+var AICurrencyDisplay = 0
+
+onready var Humantimer = get_node("Human Timer")
+
+onready var AItimer = get_node("AI Timer")
 
 func _ready():
-	timer.set_wait_time(1)
-	timer.start()
+	Humantimer.set_wait_time(1)
+	Humantimer.start()
+	
+	AItimer.set_wait_time(1)
+	AItimer.start()
+
+func _on_AI_Timer_timeout():
+	AICurrencyDisplay += 1
 
 
-func Currency_timer():
-	CurrencyDisplay += 1
+func _on_Human_Timer_timeout():
+	HumanCurrencyDisplay += 5
